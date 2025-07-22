6fc87e81adb0d30aa4045428f54bc793bc5ccf22
zewen_zhuang <zwmacproappid@163.com>
2025-07-22 11:29:56 +0800

    战法简单优化

--- External/Fitting/List/自带战法/指挥/临机制胜.py
+++ External/Fitting/List/自带战法/指挥/临机制胜.py
@@ -1,5 +1,4 @@
-
-# 战法名称: 临机制胜
+# 战法名称: 临机制胜 (使用模板系统重构版本)
 # 战法类型: 指挥
 # 战法特性: 谋略
 # 适应兵种: 盾,弓,枪,骑
@@ -13,156 +12,129 @@
 # 战斗中,敌我全体被施加异常状态时,有70%概率触发制胜:立刻对敌军随机两人造成69%谋略伤害。
 # 每回合制胜最多触发4次。累积触发4次制胜后,恢复我军全体兵力(治疗率46%,受智力影响)
 
-# 异常状态:
-# 特殊负面状态包括:震慑、缴械、技穷、混乱、嘲讽、虚弱、断粮、洪水、火攻、风暴、畏惧、妖术,共计12种
-
-from Generals.JDI_Hero import Hero
-from Generals.Enum.Generals_Enum import WeaponType
-from External.Fitting.JDI_Skill import SkillInfo, Skill
-from External.Fitting.Enum.FittingFeature_Enum import SkillFeature
-from External.Fitting.Enum.FittingType_Enum import SkillType
+from External.Fitting.List.SkillBaseTemplate import (
+    BaseSkillInfo, BaseSkillSoul, BaseSkill, get_skill_template
+)
 from External.Fitting.Enum.FittingList_Enum import Fitting_List_Enum
 from Soul.Enum.SoulResponseTime_Enum import SoulResponseTime
 from Soul.Enum.SoulSourceType_Enum import SoulSourceType
 from Soul.Enum.SoulEffectType_Enum import SoulEffectType
 from Soul.Enum.SoulDamageType_Enum import SoulDamageType
-from Soul.JDI_Soul import Soul
+from External.Fitting.Enum.FittingType_Enum import SkillType
 from Control.Log.JDI_Log import Log
 from Calcu.JDI_Calculate import *
+import random
 
-class 临机制胜_info(SkillInfo):
+class 临机制胜_info(BaseSkillInfo):
     def __init__(self):
-        self.战法名称 = Fitting_List_Enum.临机制胜
-        self.战法类型 = SkillType.指挥
-        self.战法特性 = SkillFeature.谋略
-        self.适应兵种 = [WeaponType.盾, WeaponType.弓, WeaponType.枪, WeaponType.骑]
-        self.发动率 = 1
+        # 使用工厂方法获取模板
+        template = get_skill_template('指挥_谋略', Fitting_List_Enum.临机制胜)
+        super().__init__(template)
 
-class 临机制胜_soul(Soul):
-
-    def __init__(self, 
-                 target: Hero, 
-                 initiator: Hero = None, 
-                 sourceType: SoulSourceType = SoulSourceType.不溯源, 
-                 skill: Skill = None, 
-                 response_time: SoulResponseTime = SoulResponseTime.无响应阶段, 
-                 duration: int = -1, 
-                 effect_type: SoulEffectType = SoulEffectType.无影响, 
-                 effect_value: float = 0,
-                 source_soul = None,
-                 battleField = None):
-        super().__init__(target, initiator, sourceType, skill, response_time, duration, effect_type, effect_value, source_soul, battleField)
+class 临机制胜_soul(BaseSkillSoul):
+    def __init__(self, target, initiator=None, sourceType=SoulSourceType.不溯源, 
+                 skill=None, response_time=SoulResponseTime.无响应阶段, duration=-1,
+                 effect_type=SoulEffectType.无影响, effect_value=0, source_soul=None, battleField=None):
+        super().__init__(target, initiator, sourceType, skill, response_time, duration, 
+                        effect_type, effect_value, source_soul, battleField)
         self.临机制胜发动次数 = 0
         self.临机制胜发动总次数 = 0
 
-    def handle_defeat(self, battleField=None, hero: Hero = None, sourceSoul: Soul = None):
-        
-        if hero != self.initiator:
-            return
-
-        msg_移除响应(self)
-
-    def response(self, status=SoulResponseTime.无响应阶段, battleField=None, hero: Hero = None, sourceSoul: Soul = None):
-
+    def response(self, status=SoulResponseTime.无响应阶段, battleField=None, hero=None, sourceSoul=None):
         if status == SoulResponseTime.武将溃败:
-            self.handle_defeat(battleField, hero, sourceSoul)
+            self.handle_defeat(battleField=battleField, hero=hero, sourceSoul=sourceSoul)
             return
         
         if status == SoulResponseTime.战法布阵开始时:
-            Log().show_battle_info('    [{}]发动战法【{}】'.format(self.target.get_武将名称().value, self.skill.get_战法名称().value))
+            Log().show_battle_info('    [{}]发动战法【{}】'.format(
+                self.target.get_武将名称().value, 
+                self.skill.get_战法名称().value
+            ))
 
         elif status == SoulResponseTime.回合重置阶段:
             self.临机制胜发动次数 = 0
-            
 
-        elif status == SoulResponseTime.被施加异常时 or status == SoulResponseTime.被施加控制时:
-
+        elif status in [SoulResponseTime.被施加异常时, SoulResponseTime.被施加控制时]:
             if self.临机制胜发动次数 < 4:
                 if random.random() > 0.7:
-                    Log().show_debug_info('        [{}]发动来自【{}】的[临机制胜-制胜]效果, 但因几率未触发'.format(hero.get_武将名称().value, self.skill.get_战法名称().value))
+                    Log().show_debug_info('        [{}]发动来自【{}】的[临机制胜-制胜]效果, 但因几率未触发'.format(
+                        hero.get_武将名称().value, self.skill.get_战法名称().value))
                     return
                 
-                # [张曼成]执行来自【临机制胜】的「临机制胜-制胜」效果
-                Log().show_battle_info('        [{}]执行来自【{}】的[临机制胜-制胜]效果'.format(hero.get_武将名称().value, self.skill.get_战法名称().value))
+                self._execute_制胜效果(battleField, hero)
 
-                self.临机制胜发动次数 += 1
-                self.临机制胜发动总次数 += 1
+        elif status == SoulResponseTime.被施加异常时 and self.临机制胜发动总次数 >= 4:
+            self._execute_治疗效果(battleField)
 
-                atta_hero = self.target
-                attacked_times = msg_对敌方所有目标生效_number(self.target, battleField, 2)
-                attacked_heroes = 对敌方所有目标生效(self.target, battleField)
-                for _ in range(attacked_times):
+    def _execute_制胜效果(self, battleField, hero):
+        """执行制胜效果的便捷方法"""
+        Log().show_battle_info('        [{}]执行来自【{}】的[临机制胜-制胜]效果'.format(
+            hero.get_武将名称().value, self.skill.get_战法名称().value))
 
-                    if len(attacked_heroes) == 0:
-                        break
+        self.临机制胜发动次数 += 1
+        self.临机制胜发动总次数 += 1
 
-                    attacked: Hero = 从队列确定受击武将(attacked_heroes, skill=self.skill, hero=self.target, battleField=battleField)
-                    damageModel = 计算伤害(battleField, atta_hero, attacked, SoulDamageType.谋略, SkillType.指挥, 伤害值= self.skill.临机制胜_伤害系数())
-                    damageModel.skillEffectName = "临机制胜-制胜"
+        # 对敌军随机两人造成伤害
+        attacked_times = msg_对敌方所有目标生效_number(self.target, battleField, 2)
+        attacked_heroes = 对敌方所有目标生效(self.target, battleField)
+        
+        for _ in range(attacked_times):
+            if len(attacked_heroes) == 0:
+                break
 
-                    # 创建一个伤害 SOUL
-                    damage_soul = Soul(target=attacked,
-                                        initiator=atta_hero,
-                                        sourceType=SoulSourceType.武将战法,
-                                        skill= self.skill,
-                                        effect_type=SoulEffectType.损失兵力,
-                                        effect_value=damageModel.damage_value,
-                                        source_soul=self,
-                                        battleField=battleField,
-                                        damage=damageModel)
-                    damage_soul.deploy_initial()
-                
-                if self.临机制胜发动总次数 % 4 == 0:
-                    # [周瑜]执行来自【临机制胜】的「临机制胜-恢复」效果
-                    Log().show_battle_info('        [{}]执行来自【{}】的[临机制胜-恢复]效果'.format(self.target.get_武将名称().value, self.skill.get_战法名称().value))
-                    treatment_times = msg_对己方所有目标生效_number(self.target, battleField, 3)
-                    treatment_heroes = 对己方所有目标生效(self.target, battleField)
-                    for _ in range(treatment_times):
-                        if len(treatment_heroes) == 0:
-                            break
+            attacked = 从队列确定受击武将(attacked_heroes, skill=self.skill, hero=self.target, battleField=battleField)
+            damage_soul = self.skill.create_damage_soul(
+                battleField, self.target, attacked,
+                SoulDamageType.谋略, SkillType.指挥,
+                self.skill.临机制胜_伤害系数(), self
+            )
+            damage_soul.deploy_initial()
 
-                        treatment: Hero = 从队列确定受击武将(treatment_heroes, skill=self.skill, hero=self.target, battleField=battleField)
-                        treatmentValue = 治疗计算(battleField, atta_hero, treatment, self.skill.临机制胜_治疗系数())
+    def _execute_治疗效果(self, battleField):
+        """执行治疗效果的便捷方法"""
+        Log().show_battle_info('        [{}]执行来自【{}】的[临机制胜-治疗]效果'.format(
+            self.target.get_武将名称().value, self.skill.get_战法名称().value))
 
-                        # 创建一个治疗 SOUL
-                        treatment_soul = Soul(target=treatment,
-                                              initiator=atta_hero,
-                                              effect_type=SoulEffectType.恢复兵力,
-                                              effect_value=treatmentValue,
-                                              source_soul=self,
-                                              battleField=battleField)
-                        treatment_soul.deploy_initial()
+        treatment_times = msg_对己方所有目标生效_number(self.target, battleField)
+        treatment_heroes = 对己方所有目标生效(self.target, battleField)
+        
+        for _ in range(treatment_times):
+            if len(treatment_heroes) == 0:
+                break
 
-            
+            treatment = 从队列确定受击武将(treatment_heroes, skill=self.skill, hero=self.target, battleField=battleField)
+            treatmentValue = 治疗计算(battleField, self.target, treatment, self.skill.临机制胜_治疗系数())
 
+            treatment_soul = self.skill.create_soul(
+                treatment, SoulEffectType.恢复兵力, treatmentValue
+            )
+            treatment_soul.source_soul = self
+            treatment_soul.battleField = battleField
+            treatment_soul.deploy_initial()
 
-
-class 临机制胜_skill(Skill):
-
+class 临机制胜_skill(BaseSkill):
     def __init__(self, hero, skillName):
         super().__init__(hero, skillName)
 
     def fill_init_soul(self):
-        持有者and响应者:Hero = self.get_持有者()
-        临机制胜soul = 临机制胜_soul(target=持有者and响应者, 
-                                    initiator=持有者and响应者, 
-                                    sourceType=SoulSourceType.武将战法, 
-                                    skill=self, 
-                                    response_time=SoulResponseTime.内置待响应, 
-                                    effect_type=SoulEffectType.无影响)
-        持有者and响应者.get_持有Soul列表().append(临机制胜soul)
-        持有者and响应者.get_响应Soul列表().append(临机制胜soul)
+        持有者and响应者 = self.get_持有者()
+        
+        # 使用基类的便捷方法创建Soul
+        soul = 临机制胜_soul(
+            target=持有者and响应者, 
+            initiator=持有者and响应者, 
+            sourceType=SoulSourceType.武将战法, 
+            skill=self, 
+            response_time=SoulResponseTime.内置待响应, 
+            effect_type=SoulEffectType.无影响
+        )
+        
+        持有者and响应者.get_持有Soul列表().append(soul)
+        持有者and响应者.get_响应Soul列表().append(soul)
 
+    # 使用基类的便捷方法简化数值计算
     def 临机制胜_伤害系数(self):
-        # 初始值为 60%
-        # 每一级升阶提升基础初始值为 1.4%
-        rankUp = self.get_战法升阶()
-        value = 0.6 + rankUp * 0.014
-        return value
+        return self.get_rank_bonus(0.6, 0.014)
     
     def 临机制胜_治疗系数(self):
-        # 初始值为 40%
-        # 每一级升阶提升基础初始值为 1.2%
-        rankUp = self.get_战法升阶()
-        value = 0.4 + rankUp * 0.012
-        return value
+        return self.get_rank_bonus(0.4, 0.012)
--- External/Fitting/List/自带战法/追击/膂力过人.py
+++ External/Fitting/List/自带战法/追击/膂力过人.py
@@ -1,5 +1,4 @@
-
-# 战法名称: 膂力过人
+# 战法名称: 膂力过人 (使用模板系统重构版本)
 # 战法类型: 追击
 # 战法特性: 兵刃
 # 适应兵种: 盾,弓,枪,骑
@@ -11,142 +10,79 @@
 # 满阶膂力过人:
 # 普通攻击后,对当前攻击目标造成172.5%兵刃伤害,若目标武力低于自身额外造成90.5%兵刃伤害
 
-from Generals.JDI_Hero import Hero
-from Generals.Enum.Generals_Enum import WeaponType
-from External.Fitting.JDI_Skill import SkillInfo, Skill
-from External.Fitting.Enum.FittingFeature_Enum import SkillFeature
-from External.Fitting.Enum.FittingType_Enum import SkillType
+from External.Fitting.List.SkillBaseTemplate import (
+    BaseSkillInfo, BaseSkillSoul, BaseSkill, get_skill_template
+)
 from External.Fitting.Enum.FittingList_Enum import Fitting_List_Enum
 from Soul.Enum.SoulResponseTime_Enum import SoulResponseTime
 from Soul.Enum.SoulSourceType_Enum import SoulSourceType
 from Soul.Enum.SoulEffectType_Enum import SoulEffectType
 from Soul.Enum.SoulDamageType_Enum import SoulDamageType
-from Soul.JDI_Soul import Soul
+from External.Fitting.Enum.FittingType_Enum import SkillType
 from Control.Log.JDI_Log import Log
-from Calcu.JDI_Calculate import *
+from Calcu.JDI_Calculate import 计算伤害
 
-class 膂力过人_info(SkillInfo):
+class 膂力过人_info(BaseSkillInfo):
     def __init__(self):
-        self.战法名称 = Fitting_List_Enum.膂力过人
-        self.战法类型 = SkillType.追击
-        self.战法特性 = SkillFeature.兵刃
-        self.适应兵种 = [WeaponType.盾, WeaponType.弓, WeaponType.枪, WeaponType.骑]
-        self.发动率 = 1
+        # 使用工厂方法获取模板
+        template = get_skill_template('追击_兵刃', Fitting_List_Enum.膂力过人)
+        super().__init__(template)
 
-class 膂力过人_soul(Soul):
-    def __init__(self, 
-                 target: Hero, 
-                 initiator: Hero = None, 
-                 sourceType: SoulSourceType = SoulSourceType.不溯源, 
-                 skill: Skill = None, 
-                 response_time: SoulResponseTime = SoulResponseTime.无响应阶段, 
-                 duration: int = -1, 
-                 effect_type: SoulEffectType = SoulEffectType.无影响, 
-                 effect_value: float = 0,
-                 source_soul = None,
-                 battleField = None):
-        super().__init__(target, initiator, sourceType, skill, response_time, duration, effect_type, effect_value, source_soul, battleField)
-        self.soul持有列表 = []
-
-    def handle_defeat(self, battleField=None, hero: Hero = None, sourceSoul: Soul = None):
-        
-        if hero != self.initiator:
-            return
-        
-        soul_to_remove = []
-        for soul in self.soul持有列表:
-            if soul.target == self.target:
-                soul_to_remove.append(soul)
-        for soul in soul_to_remove:
-            self.soul持有列表.remove(soul)
-
-        if self.soul持有列表.__len__() <= 0:
-            return
-
-        soul_to_remove = []
-        for soul in self.soul持有列表:
-            if soul.initiator == self.target:
-                soul.restore_initial()
-                soul_to_remove.append(soul)
-
-        for soul in self.soul持有列表:
-            if soul.initiator == self.target:
-                if soul in self.soul持有列表:
-                    self.soul持有列表.remove(soul)
-                if soul in self.soul持有列表:
-                    self.soul持有列表.remove(soul)
-
-        msg_移除响应(self)
-
-    def response(self, status = SoulResponseTime.无响应阶段, battleField=None, hero = None, sourceSoul=None):
-
+class 膂力过人_soul(BaseSkillSoul):
+    def response(self, status=SoulResponseTime.无响应阶段, battleField=None, hero=None, sourceSoul=None):
         if status == SoulResponseTime.武将溃败:
             self.handle_defeat(battleField=battleField, hero=hero, sourceSoul=sourceSoul)
             return
         
         if status == SoulResponseTime.追击行动时 and self.target == hero:
-            Log().show_battle_info('    [{}]发动战法【{}】'.format(self.target.get_武将名称().value, self.skill.get_战法名称().value))
+            Log().show_battle_info('    [{}]发动战法【{}】'.format(
+                self.target.get_武将名称().value, 
+                self.skill.get_战法名称().value
+            ))
+            
             attacked_hero = sourceSoul.target
-            damage_class: Damage = 计算伤害(battleField, 
-                                        self.target, 
-                                        attacked_hero, 
-                                        SoulDamageType.兵刃, 
-                                        SkillType.追击, 
-                                        self.skill.膂力过人_伤害系数())
-            damage_soul = Soul(target=attacked_hero,
-                                initiator=self.target,
-                                sourceType=SoulSourceType.武将战法,
-                                skill=self.skill,
-                                effect_type=SoulEffectType.损失兵力,
-                                effect_value=damage_class.damage_value,
-                                source_soul=self,
-                                battleField=battleField,
-                                damage=damage_class)
+            
+            # 基础伤害
+            damage_soul = self.skill.create_damage_soul(
+                battleField, self.target, attacked_hero, 
+                SoulDamageType.兵刃, SkillType.追击, 
+                self.skill.膂力过人_伤害系数(), self
+            )
             damage_soul.deploy_initial()
 
+            # 额外伤害判定
             if attacked_hero.get_武力() < self.target.get_武力():
-
-                extra_damage_class: Damage = 计算伤害(battleField,
-                                                self.target,
-                                                attacked_hero,
-                                                SoulDamageType.兵刃,
-                                                SkillType.追击,
-                                                self.skill.膂力过人_额外伤害系数())
-                extra_damage_class.skillEffectName = "膂力过人"
-                Log().show_battle_info('    [{}]执行来自【{}】的[{}]效果'.format(attacked_hero.get_武将名称().value, self.skill.get_战法名称().value, extra_damage_class.skillEffectName))
-
-                extra_damage_soul = Soul(target=attacked_hero,
-                                        initiator=self.target,
-                                        sourceType=SoulSourceType.武将战法,
-                                        skill=self.skill,
-                                        effect_type=SoulEffectType.损失兵力,
-                                        effect_value=extra_damage_class.damage_value,
-                                        source_soul=self,
-                                        battleField=battleField,
-                                        damage=extra_damage_class)
+                extra_damage_soul = self.skill.create_damage_soul(
+                    battleField, self.target, attacked_hero,
+                    SoulDamageType.兵刃, SkillType.追击,
+                    self.skill.膂力过人_额外伤害系数(), self,
+                    effect_name="膂力过人"
+                )
                 extra_damage_soul.deploy_initial()
 
-class 膂力过人_skill(Skill):
+class 膂力过人_skill(BaseSkill):
     def __init__(self, hero, skillName):
         super().__init__(hero, skillName)
 
     def fill_init_soul(self):
-        持有者and响应者:Hero = self.get_持有者()
-        膂力过人soul = 膂力过人_soul(target=持有者and响应者, 
-                             initiator=持有者and响应者, 
-                             sourceType=SoulSourceType.武将战法, 
-                             skill=self, 
-                             response_time=SoulResponseTime.内置待响应, 
-                             effect_type=SoulEffectType.无影响)
-        持有者and响应者.get_持有Soul列表().append(膂力过人soul)
-        持有者and响应者.get_响应Soul列表().append(膂力过人soul)
+        持有者and响应者 = self.get_持有者()
+        
+        # 使用基类的便捷方法创建Soul
+        soul = 膂力过人_soul(
+            target=持有者and响应者, 
+            initiator=持有者and响应者, 
+            sourceType=SoulSourceType.武将战法, 
+            skill=self, 
+            response_time=SoulResponseTime.内置待响应, 
+            effect_type=SoulEffectType.无影响
+        )
+        
+        持有者and响应者.get_持有Soul列表().append(soul)
+        持有者and响应者.get_响应Soul列表().append(soul)
 
+    # 使用基类的便捷方法简化数值计算
     def 膂力过人_伤害系数(self):
-        rankUp = self.get_战法升阶()
-        value = 1.5 + rankUp * 0.045
-        return value
+        return self.get_rank_bonus(1.5, 0.045)
     
     def 膂力过人_额外伤害系数(self):
-        rankUp = self.get_战法升阶()
-        value = 0.7 + rankUp * 0.021
-        return value
+        return self.get_rank_bonus(0.7, 0.021)

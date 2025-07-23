cd7b2767286bab49886cf415875cf78c3c1bc214
zhuangzewen <zwmacproappid@163.com>
2025-07-24 04:06:58 +0800

    华佗

--- Generals/List/华佗.py
+++ Generals/List/华佗.py
@@ -2,15 +2,15 @@
 # HeroInfoKey.武将阵营:Faction.汉,
 # HeroInfoKey.武将兵种:WeaponType.弓,
 # HeroInfoKey.武将性别:1,
-# HeroInfoKey.初始武力:60,
-# HeroInfoKey.武力成长:0.80,
-# HeroInfoKey.初始智力:120,
-# HeroInfoKey.智力成长:2.80,
-# HeroInfoKey.初始统帅:85,
-# HeroInfoKey.统帅成长:1.90,
-# HeroInfoKey.初始先攻:70,
-# HeroInfoKey.先攻成长:2.00,
-# HeroInfoKey.自带战法:SkillName.妙手回春
+# HeroInfoKey.初始武力:13,
+# HeroInfoKey.武力成长:0.35,
+# HeroInfoKey.初始智力:106,
+# HeroInfoKey.智力成长:2.35,
+# HeroInfoKey.初始统帅:68,
+# HeroInfoKey.统帅成长:1.60,
+# HeroInfoKey.初始先攻:55,
+# HeroInfoKey.先攻成长:1.73,
+# HeroInfoKey.自带战法:麻沸散
 
 from Generals.JDI_Hero import HeroInfo
 from Control.Imports.hero_imports import Generals_Name_Enum, Faction, WeaponType, Fitting_List_Enum
@@ -21,13 +21,13 @@ class 华佗_info(HeroInfo):
         self.武将阵营 = Faction.汉
         self.武将兵种 = WeaponType.弓
         self.武将性别 = 1
-        self.初始武力 = 60
-        self.武力成长 = 0.80
-        self.初始智力 = 120
-        self.智力成长 = 2.80
-        self.初始统帅 = 85
-        self.统帅成长 = 1.90
-        self.初始先攻 = 70
-        self.先攻成长 = 2.00
-        self.自带战法 = Fitting_List_Enum.妙手回春
+        self.初始武力 = 13
+        self.武力成长 = 0.35
+        self.初始智力 = 106
+        self.智力成长 = 2.35
+        self.初始统帅 = 68
+        self.统帅成长 = 1.60
+        self.初始先攻 = 55
+        self.先攻成长 = 1.73
+        self.自带战法 = Fitting_List_Enum.麻沸散
         self.缘分列表 = []

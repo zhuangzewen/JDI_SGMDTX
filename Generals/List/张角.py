df68bc68c74787146996bc4ebca1bc80c1ef011d
zhuangzewen <zwmacproappid@163.com>
2025-07-24 04:26:33 +0800

    张角

--- Generals/List/张角.py
+++ Generals/List/张角.py
@@ -1,15 +1,16 @@
 # HeroInfoKey.武将名称:HeroName.张角,
 # HeroInfoKey.武将阵营:Faction.群,
-# HeroInfoKey.武将兵种:WeaponType.弓,
+# HeroInfoKey.武将兵种:WeaponType.盾,
 # HeroInfoKey.武将性别:1,
 # HeroInfoKey.初始武力:35,
-# HeroInfoKey.武力成长:1.0,
-# HeroInfoKey.初始智力:108,
-# HeroInfoKey.智力成长:2.7,
-# HeroInfoKey.初始统帅:78,
-# HeroInfoKey.统帅成长:1.8,
+# HeroInfoKey.武力成长:0.43,
+# HeroInfoKey.初始智力:113,
+# HeroInfoKey.智力成长:2.55,
+# HeroInfoKey.初始统帅:105,
+# HeroInfoKey.统帅成长:1.99,
 # HeroInfoKey.初始先攻:62,
-# HeroInfoKey.先攻成长:1.3,
+# HeroInfoKey.先攻成长:1.53,
+# HeroInfoKey.自带战法:黄天当立,
 
 from Generals.JDI_Hero import HeroInfo
 from Control.Imports.hero_imports import Generals_Name_Enum, Faction, WeaponType, Fitting_List_Enum
@@ -18,14 +19,15 @@ class 张角_info(HeroInfo):
     def __init__(self):
         self.武将名称 = Generals_Name_Enum.张角
         self.武将阵营 = Faction.群
-        self.武将兵种 = WeaponType.弓
+        self.武将兵种 = WeaponType.盾
         self.武将性别 = 1
         self.初始武力 = 35
-        self.武力成长 = 1.0
-        self.初始智力 = 108
-        self.智力成长 = 2.7
-        self.初始统帅 = 78
-        self.统帅成长 = 1.8
+        self.武力成长 = 0.43
+        self.初始智力 = 113
+        self.智力成长 = 2.55
+        self.初始统帅 = 105
+        self.统帅成长 = 1.99
         self.初始先攻 = 62
-        self.先攻成长 = 1.3
+        self.先攻成长 = 1.53
         self.缘分列表 = []
+        self.自带战法 = Fitting_List_Enum.黄天当立

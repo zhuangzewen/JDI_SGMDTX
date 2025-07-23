3f695ff3f902e9a7057b70d4b4174657058f6311
zhuangzewen <zwmacproappid@163.com>
2025-07-24 04:01:24 +0800

    庞德

--- Generals/List/庞德.py
+++ Generals/List/庞德.py
@@ -1,15 +1,15 @@
 # HeroInfoKey.武将名称:HeroName.庞德,
 # HeroInfoKey.武将阵营:Faction.群,
-# HeroInfoKey.武将兵种:WeaponType.刀,
+# HeroInfoKey.武将兵种:WeaponType.骑,
 # HeroInfoKey.武将性别:1,
-# HeroInfoKey.初始武力:95,
-# HeroInfoKey.武力成长:2.2,
-# HeroInfoKey.初始智力:68,
-# HeroInfoKey.智力成长:1.5,
-# HeroInfoKey.初始统帅:82,
-# HeroInfoKey.统帅成长:1.8,
-# HeroInfoKey.初始先攻:85,
-# HeroInfoKey.先攻成长:1.8,
+# HeroInfoKey.初始武力:106,
+# HeroInfoKey.武力成长:2.40,
+# HeroInfoKey.初始智力:71,
+# HeroInfoKey.智力成长:0.78,
+# HeroInfoKey.初始统帅:93,
+# HeroInfoKey.统帅成长:1.69,
+# HeroInfoKey.初始先攻:73,
+# HeroInfoKey.先攻成长:2.69,
 
 from Generals.JDI_Hero import HeroInfo
 from Control.Imports.hero_imports import Generals_Name_Enum, Faction, WeaponType, Fitting_List_Enum
@@ -18,14 +18,15 @@ class 庞德_info(HeroInfo):
     def __init__(self):
         self.武将名称 = Generals_Name_Enum.庞德
         self.武将阵营 = Faction.群
-        self.武将兵种 = WeaponType.刀
+        self.武将兵种 = WeaponType.骑
         self.武将性别 = 1
-        self.初始武力 = 95
-        self.武力成长 = 2.2
-        self.初始智力 = 68
-        self.智力成长 = 1.5
-        self.初始统帅 = 82
-        self.统帅成长 = 1.8
-        self.初始先攻 = 85
-        self.先攻成长 = 1.8
+        self.初始武力 = 106
+        self.武力成长 = 2.40
+        self.初始智力 = 71
+        self.智力成长 = 0.78
+        self.初始统帅 = 93
+        self.统帅成长 = 1.69
+        self.初始先攻 = 73
+        self.先攻成长 = 2.69
+        self.自带战法 = Fitting_List_Enum.陷阵踏难
         self.缘分列表 = []

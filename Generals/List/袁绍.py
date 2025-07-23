f6dc8cd5064be7b5add174a8fcc561ac57299f71
zhuangzewen <zwmacproappid@163.com>
2025-07-24 04:48:34 +0800

    袁绍

--- Generals/List/袁绍.py
+++ Generals/List/袁绍.py
@@ -1,15 +1,16 @@
 # HeroInfoKey.武将名称:HeroName.袁绍,
 # HeroInfoKey.武将阵营:Faction.群,
 # HeroInfoKey.武将兵种:WeaponType.枪,
-# HeroInfoKey.武将性别:1,
-# HeroInfoKey.初始武力:75,
-# HeroInfoKey.武力成长:1.6,
-# HeroInfoKey.初始智力:85,
-# HeroInfoKey.智力成长:2.0,
-# HeroInfoKey.初始统帅:98,
-# HeroInfoKey.统帅成长:2.3,
-# HeroInfoKey.初始先攻:65,
-# HeroInfoKey.先攻成长:1.4,
+# HeroInfoKey.武将兵种:WeaponType.弓,
+# HeroInfoKey.初始武力:80,
+# HeroInfoKey.武力成长:2.00,
+# HeroInfoKey.初始智力:92,
+# HeroInfoKey.智力成长:1.80,
+# HeroInfoKey.初始统帅:105,
+# HeroInfoKey.统帅成长:2.03,
+# HeroInfoKey.初始先攻:68,
+# HeroInfoKey.先攻成长:2.25,
+# HeroInfoKey.自带战法:Fitting_List_Enum.合聚群雄,
 
 from Generals.JDI_Hero import HeroInfo
 from Control.Imports.hero_imports import Generals_Name_Enum, Faction, WeaponType, Fitting_List_Enum
@@ -18,14 +19,15 @@ class 袁绍_info(HeroInfo):
     def __init__(self):
         self.武将名称 = Generals_Name_Enum.袁绍
         self.武将阵营 = Faction.群
-        self.武将兵种 = WeaponType.枪
+        self.武将兵种 = WeaponType.弓
         self.武将性别 = 1
-        self.初始武力 = 75
-        self.武力成长 = 1.6
-        self.初始智力 = 85
-        self.智力成长 = 2.0
-        self.初始统帅 = 98
-        self.统帅成长 = 2.3
-        self.初始先攻 = 65
-        self.先攻成长 = 1.4
+        self.初始武力 = 80
+        self.武力成长 = 2.00
+        self.初始智力 = 92
+        self.智力成长 = 1.80
+        self.初始统帅 = 105
+        self.统帅成长 = 2.03
+        self.初始先攻 = 68
+        self.先攻成长 = 2.25
         self.缘分列表 = []
+        self.自带战法 = Fitting_List_Enum.合聚群雄

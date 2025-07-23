c441fb8a6df2afa7363da3157f5ce7f1a5045f5e
zhuangzewen <zwmacproappid@163.com>
2025-07-24 01:39:05 +0800

    郭图

--- Generals/List/郭图.py
+++ Generals/List/郭图.py
@@ -18,7 +18,7 @@ from Control.Imports.hero_imports import Generals_Name_Enum, Faction, WeaponType
 class 郭图_info(HeroInfo):
     def __init__(self):
         self.武将名称 = Generals_Name_Enum.郭图
-        self.武将阵营 = Faction.魏
+        self.武将阵营 = Faction.群
         self.武将兵种 = WeaponType.枪
         self.武将性别 = 1
         self.初始武力 = 51

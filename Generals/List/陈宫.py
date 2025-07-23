f502327c6efd2a4b620207e2707f29fc8e6b2975
zhuangzewen <zwmacproappid@163.com>
2025-07-24 04:21:22 +0800

    陈宫

--- Generals/List/陈宫.py
+++ Generals/List/陈宫.py
@@ -2,14 +2,15 @@
 # HeroInfoKey.武将阵营:Faction.群,
 # HeroInfoKey.武将兵种:WeaponType.弓,
 # HeroInfoKey.武将性别:1,
-# HeroInfoKey.初始武力:52,
-# HeroInfoKey.武力成长:1.2,
-# HeroInfoKey.初始智力:98,
-# HeroInfoKey.智力成长:2.4,
-# HeroInfoKey.初始统帅:88,
-# HeroInfoKey.统帅成长:2.0,
-# HeroInfoKey.初始先攻:68,
-# HeroInfoKey.先攻成长:1.4,
+# HeroInfoKey.初始武力:55,
+# HeroInfoKey.武力成长:0.63,
+# HeroInfoKey.初始智力:110,
+# HeroInfoKey.智力成长:2.54,
+# HeroInfoKey.初始统帅:86,
+# HeroInfoKey.统帅成长:1.87,
+# HeroInfoKey.初始先攻:73,
+# HeroInfoKey.先攻成长:1.94,
+# HeroInfoKey.自带战法:SkillName.智令从计
 
 from Generals.JDI_Hero import HeroInfo
 from Control.Imports.hero_imports import Generals_Name_Enum, Faction, WeaponType, Fitting_List_Enum
@@ -20,12 +21,13 @@ class 陈宫_info(HeroInfo):
         self.武将阵营 = Faction.群
         self.武将兵种 = WeaponType.弓
         self.武将性别 = 1
-        self.初始武力 = 52
-        self.武力成长 = 1.2
-        self.初始智力 = 98
-        self.智力成长 = 2.4
-        self.初始统帅 = 88
-        self.统帅成长 = 2.0
-        self.初始先攻 = 68
-        self.先攻成长 = 1.4
+        self.初始武力 = 55
+        self.武力成长 = 0.63
+        self.初始智力 = 110
+        self.智力成长 = 2.54
+        self.初始统帅 = 86
+        self.统帅成长 = 1.87
+        self.初始先攻 = 73
+        self.先攻成长 = 1.94
+        self.自带战法 = Fitting_List_Enum.智令从计
         self.缘分列表 = []

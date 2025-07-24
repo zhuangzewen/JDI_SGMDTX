1375e7596cf75cfae581eecd398a236506fa3869
zewen_zhuang <zwmacproappid@163.com>
2025-07-24 14:43:37 +0800

    feat(曹仁): 更新曹仁属性并添加自带战法
    调整曹仁的基础属性和成长值，并添加自带战法"固镇襄樊"

--- Generals/List/曹仁.py
+++ Generals/List/曹仁.py
@@ -2,14 +2,14 @@
 # HeroInfoKey.武将阵营:Faction.魏,
 # HeroInfoKey.武将兵种:WeaponType.盾,
 # HeroInfoKey.武将性别:1,
-# HeroInfoKey.初始武力:85,
-# HeroInfoKey.武力成长:1.8,
-# HeroInfoKey.初始智力:70,
-# HeroInfoKey.智力成长:1.5,
-# HeroInfoKey.初始统帅:95,
-# HeroInfoKey.统帅成长:2.2,
-# HeroInfoKey.初始先攻:65,
-# HeroInfoKey.先攻成长:1.4,
+# HeroInfoKey.初始武力:81,
+# HeroInfoKey.武力成长:1.35,
+# HeroInfoKey.初始智力:79,
+# HeroInfoKey.智力成长:1.53,
+# HeroInfoKey.初始统帅:113,
+# HeroInfoKey.统帅成长:2.35,
+# HeroInfoKey.初始先攻:44,
+# HeroInfoKey.先攻成长:1.55,
 
 from Generals.JDI_Hero import HeroInfo
 from Control.Imports.hero_imports import Generals_Name_Enum, Faction, WeaponType, Fitting_List_Enum
@@ -20,12 +20,13 @@ class 曹仁_info(HeroInfo):
         self.武将阵营 = Faction.魏
         self.武将兵种 = WeaponType.盾
         self.武将性别 = 1
-        self.初始武力 = 85
-        self.武力成长 = 1.8
-        self.初始智力 = 70
-        self.智力成长 = 1.5
-        self.初始统帅 = 95
-        self.统帅成长 = 2.2
-        self.初始先攻 = 65
-        self.先攻成长 = 1.4
+        self.初始武力 = 81
+        self.武力成长 = 1.35
+        self.初始智力 = 79
+        self.智力成长 = 1.53
+        self.初始统帅 = 113
+        self.统帅成长 = 2.35
+        self.初始先攻 = 44
+        self.先攻成长 = 1.55
         self.缘分列表 = []
+        self.自带战法 = Fitting_List_Enum.固镇襄樊

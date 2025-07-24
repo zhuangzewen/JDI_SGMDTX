1b9179258bb189ca82ed1148fa02e02b2f5f2af2
zewen_zhuang <zwmacproappid@163.com>
2025-07-24 12:25:34 +0800

    张郃

--- Generals/List/张郃.py
+++ Generals/List/张郃.py
@@ -2,14 +2,14 @@
 # HeroInfoKey.武将阵营:Faction.魏,
 # HeroInfoKey.武将兵种:WeaponType.枪,
 # HeroInfoKey.武将性别:1,
-# HeroInfoKey.初始武力:92,
-# HeroInfoKey.武力成长:2.1,
-# HeroInfoKey.初始智力:80,
-# HeroInfoKey.智力成长:1.7,
-# HeroInfoKey.初始统帅:88,
-# HeroInfoKey.统帅成长:1.9,
-# HeroInfoKey.初始先攻:85,
-# HeroInfoKey.先攻成长:1.8,
+# HeroInfoKey.初始武力:104,
+# HeroInfoKey.武力成长:2.59,
+# HeroInfoKey.初始智力:81,
+# HeroInfoKey.智力成长:0.77,
+# HeroInfoKey.初始统帅:101,
+# HeroInfoKey.统帅成长:1.91,
+# HeroInfoKey.初始先攻:67,
+# HeroInfoKey.先攻成长:1.93,
 
 from Generals.JDI_Hero import HeroInfo
 from Control.Imports.hero_imports import Generals_Name_Enum, Faction, WeaponType, Fitting_List_Enum
@@ -20,12 +20,13 @@ class 张郃_info(HeroInfo):
         self.武将阵营 = Faction.魏
         self.武将兵种 = WeaponType.枪
         self.武将性别 = 1
-        self.初始武力 = 92
-        self.武力成长 = 2.1
-        self.初始智力 = 80
-        self.智力成长 = 1.7
-        self.初始统帅 = 88
-        self.统帅成长 = 1.9
-        self.初始先攻 = 85
-        self.先攻成长 = 1.8
+        self.初始武力 = 104
+        self.武力成长 = 2.59
+        self.初始智力 = 81
+        self.智力成长 = 0.77
+        self.初始统帅 = 101
+        self.统帅成长 = 1.91
+        self.初始先攻 = 67
+        self.先攻成长 = 1.93
         self.缘分列表 = []
+        self.自带战法 = [Fitting_List_Enum.大破街亭]

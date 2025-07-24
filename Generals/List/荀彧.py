e154abf91417c380e571dc106526d39a70f8d1e3
zewen_zhuang <zwmacproappid@163.com>
2025-07-24 12:13:37 +0800

    荀彧

--- Generals/List/荀彧.py
+++ Generals/List/荀彧.py
@@ -2,14 +2,14 @@
 # HeroInfoKey.武将阵营:Faction.魏,
 # HeroInfoKey.武将兵种:WeaponType.弓,
 # HeroInfoKey.武将性别:1,
-# HeroInfoKey.初始武力:45,
-# HeroInfoKey.武力成长:1.0,
-# HeroInfoKey.初始智力:105,
-# HeroInfoKey.智力成长:2.6,
-# HeroInfoKey.初始统帅:92,
-# HeroInfoKey.统帅成长:2.1,
-# HeroInfoKey.初始先攻:68,
-# HeroInfoKey.先攻成长:1.4,
+# HeroInfoKey.初始武力:37,
+# HeroInfoKey.武力成长:0.19,
+# HeroInfoKey.初始智力:120,
+# HeroInfoKey.智力成长:2.70,
+# HeroInfoKey.初始统帅:78,
+# HeroInfoKey.统帅成长:1.60,
+# HeroInfoKey.初始先攻:57,
+# HeroInfoKey.先攻成长:1.65,
 
 from Generals.JDI_Hero import HeroInfo
 from Control.Imports.hero_imports import Generals_Name_Enum, Faction, WeaponType, Fitting_List_Enum
@@ -20,12 +20,13 @@ class 荀彧_info(HeroInfo):
         self.武将阵营 = Faction.魏
         self.武将兵种 = WeaponType.弓
         self.武将性别 = 1
-        self.初始武力 = 45
-        self.武力成长 = 1.0
-        self.初始智力 = 105
-        self.智力成长 = 2.6
-        self.初始统帅 = 92
-        self.统帅成长 = 2.1
-        self.初始先攻 = 68
-        self.先攻成长 = 1.4
+        self.初始武力 = 37
+        self.武力成长 = 0.19
+        self.初始智力 = 120
+        self.智力成长 = 2.70
+        self.初始统帅 = 78
+        self.统帅成长 = 1.60
+        self.初始先攻 = 57
+        self.先攻成长 = 1.65
+        self.自带战法 = Fitting_List_Enum.建计举人
         self.缘分列表 = []

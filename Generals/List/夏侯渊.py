c4d5e55a7101d42ea1bc409ff594e51ebc9e4bd4
zewen_zhuang <zwmacproappid@163.com>
2025-07-24 14:25:37 +0800

    feat: 更新夏侯渊属性并修改兵种为骑兵
    修改夏侯渊的兵种从弓兵变为骑兵，并调整其各项属性数值
    添加自带战法"神速奔袭"
    同时更新确认记录文档中的相关信息

--- Generals/List/夏侯渊.py
+++ Generals/List/夏侯渊.py
@@ -1,15 +1,16 @@
 # HeroInfoKey.武将名称:HeroName.夏侯渊,
 # HeroInfoKey.武将阵营:Faction.魏,
-# HeroInfoKey.武将兵种:WeaponType.弓,
+# HeroInfoKey.武将兵种:WeaponType.骑,
 # HeroInfoKey.武将性别:1,
-# HeroInfoKey.初始武力:90,
-# HeroInfoKey.武力成长:2.0,
-# HeroInfoKey.初始智力:72,
-# HeroInfoKey.智力成长:1.6,
-# HeroInfoKey.初始统帅:85,
-# HeroInfoKey.统帅成长:1.8,
-# HeroInfoKey.初始先攻:95,
-# HeroInfoKey.先攻成长:2.2,
+# HeroInfoKey.初始武力:107,
+# HeroInfoKey.武力成长:2.40,
+# HeroInfoKey.初始智力:59,
+# HeroInfoKey.智力成长:0.75,
+# HeroInfoKey.初始统帅:101,
+# HeroInfoKey.统帅成长:1.74,
+# HeroInfoKey.初始先攻:94,
+# HeroInfoKey.先攻成长:2.81,
+# HeroInfoKey.自带战法:Fitting_List_Enum.神速奔袭,
 
 from Generals.JDI_Hero import HeroInfo
 from Control.Imports.hero_imports import Generals_Name_Enum, Faction, WeaponType, Fitting_List_Enum
@@ -18,14 +19,15 @@ class 夏侯渊_info(HeroInfo):
     def __init__(self):
         self.武将名称 = Generals_Name_Enum.夏侯渊
         self.武将阵营 = Faction.魏
-        self.武将兵种 = WeaponType.弓
+        self.武将兵种 = WeaponType.骑
         self.武将性别 = 1
-        self.初始武力 = 90
-        self.武力成长 = 2.0
-        self.初始智力 = 72
-        self.智力成长 = 1.6
-        self.初始统帅 = 85
-        self.统帅成长 = 1.8
-        self.初始先攻 = 95
-        self.先攻成长 = 2.2
+        self.初始武力 = 107
+        self.武力成长 = 2.40
+        self.初始智力 = 59
+        self.智力成长 = 0.75
+        self.初始统帅 = 101
+        self.统帅成长 = 1.74
+        self.初始先攻 = 94
+        self.先攻成长 = 2.81
+        self.自带战法 = Fitting_List_Enum.神速奔袭
         self.缘分列表 = []

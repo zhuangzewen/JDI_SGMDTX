9f2115dc9cd5ed27368aa98ea05a893eed1f2ab1
zewen_zhuang <zwmacproappid@163.com>
2025-07-24 14:52:18 +0800

    feat(甄洛): 更新甄洛武将属性和战法信息
    调整甄洛的基础属性和成长值，并添加自带战法"流风回雪"

--- Generals/List/甄洛.py
+++ Generals/List/甄洛.py
@@ -2,14 +2,15 @@
 # HeroInfoKey.武将阵营:Faction.魏,
 # HeroInfoKey.武将兵种:WeaponType.弓,
 # HeroInfoKey.武将性别:0,
-# HeroInfoKey.初始武力:38,
-# HeroInfoKey.武力成长:1.0,
-# HeroInfoKey.初始智力:88,
-# HeroInfoKey.智力成长:2.2,
-# HeroInfoKey.初始统帅:75,
-# HeroInfoKey.统帅成长:1.8,
-# HeroInfoKey.初始先攻:72,
-# HeroInfoKey.先攻成长:1.6,
+# HeroInfoKey.初始武力:22,
+# HeroInfoKey.武力成长:0.23,
+# HeroInfoKey.初始智力:103,
+# HeroInfoKey.智力成长:2.16,
+# HeroInfoKey.初始统帅:103,
+# HeroInfoKey.统帅成长:1.62,
+# HeroInfoKey.初始先攻:56,
+# HeroInfoKey.先攻成长:1.55,
+# HeroInfoKey.自带战法:Fitting_List_Enum.流风回雪,
 
 from Generals.JDI_Hero import HeroInfo
 from Control.Imports.hero_imports import Generals_Name_Enum, Faction, WeaponType, Fitting_List_Enum
@@ -20,12 +21,13 @@ class 甄洛_info(HeroInfo):
         self.武将阵营 = Faction.魏
         self.武将兵种 = WeaponType.弓
         self.武将性别 = 0
-        self.初始武力 = 38
-        self.武力成长 = 1.0
-        self.初始智力 = 88
-        self.智力成长 = 2.2
-        self.初始统帅 = 75
-        self.统帅成长 = 1.8
-        self.初始先攻 = 72
-        self.先攻成长 = 1.6
+        self.初始武力 = 22
+        self.武力成长 = 0.23
+        self.初始智力 = 103
+        self.智力成长 = 2.16
+        self.初始统帅 = 103
+        self.统帅成长 = 1.62
+        self.初始先攻 = 56
+        self.先攻成长 = 1.55
         self.缘分列表 = []
+        self.自带战法 = Fitting_List_Enum.流风回雪

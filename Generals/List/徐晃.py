9687e5bcd964d93ff85f8860ab0720570394d1f7
zewen_zhuang <zwmacproappid@163.com>
2025-07-24 12:40:13 +0800

    徐晃

--- Generals/List/徐晃.py
+++ Generals/List/徐晃.py
@@ -18,14 +18,15 @@ class 徐晃_info(HeroInfo):
     def __init__(self):
         self.武将名称 = Generals_Name_Enum.徐晃
         self.武将阵营 = Faction.魏
-        self.武将兵种 = WeaponType.骑
+        self.武将兵种 = WeaponType.盾
         self.武将性别 = 1
-        self.初始武力 = 95
-        self.武力成长 = 2.2
-        self.初始智力 = 75
-        self.智力成长 = 1.6
-        self.初始统帅 = 90
-        self.统帅成长 = 2.0
-        self.初始先攻 = 80
-        self.先攻成长 = 1.7
+        self.初始武力 = 105
+        self.武力成长 = 2.58
+        self.初始智力 = 79
+        self.智力成长 = 0.88
+        self.初始统帅 = 106
+        self.统帅成长 = 1.75
+        self.初始先攻 = 75
+        self.先攻成长 = 2.12
         self.缘分列表 = []
+        self.自带战法 = Fitting_List_Enum.长驱直入

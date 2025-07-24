8c1f8ab038d2700c03f6c72ad97bc0d35469c55a
zewen_zhuang <zwmacproappid@163.com>
2025-07-24 12:02:58 +0800

    典韦

--- Generals/List/典韦.py
+++ Generals/List/典韦.py
@@ -1,15 +1,15 @@
 # HeroInfoKey.武将名称:HeroName.典韦,
 # HeroInfoKey.武将阵营:Faction.魏,
-# HeroInfoKey.武将兵种:WeaponType.盾,
+# HeroInfoKey.武将兵种:WeaponType.枪,
 # HeroInfoKey.武将性别:1,
-# HeroInfoKey.初始武力:105,
-# HeroInfoKey.武力成长:2.5,
-# HeroInfoKey.初始智力:45,
-# HeroInfoKey.智力成长:1.0,
-# HeroInfoKey.初始统帅:85,
-# HeroInfoKey.统帅成长:1.8,
-# HeroInfoKey.初始先攻:75,
-# HeroInfoKey.先攻成长:1.7,
+# HeroInfoKey.初始武力:120,
+# HeroInfoKey.武力成长:2.78,
+# HeroInfoKey.初始智力:42,
+# HeroInfoKey.智力成长:0.37,
+# HeroInfoKey.初始统帅:114,
+# HeroInfoKey.统帅成长:1.75,
+# HeroInfoKey.初始先攻:65,
+# HeroInfoKey.先攻成长:2.30,
 
 from Generals.JDI_Hero import HeroInfo
 from Control.Imports.hero_imports import Generals_Name_Enum, Faction, WeaponType, Fitting_List_Enum
@@ -18,14 +18,15 @@ class 典韦_info(HeroInfo):
     def __init__(self):
         self.武将名称 = Generals_Name_Enum.典韦
         self.武将阵营 = Faction.魏
-        self.武将兵种 = WeaponType.盾
+        self.武将兵种 = WeaponType.枪
         self.武将性别 = 1
-        self.初始武力 = 105
-        self.武力成长 = 2.5
-        self.初始智力 = 45
-        self.智力成长 = 1.0
-        self.初始统帅 = 85
-        self.统帅成长 = 1.8
-        self.初始先攻 = 75
-        self.先攻成长 = 1.7
+        self.初始武力 = 120
+        self.武力成长 = 2.78
+        self.初始智力 = 42
+        self.智力成长 = 0.37
+        self.初始统帅 = 114
+        self.统帅成长 = 1.75
+        self.初始先攻 = 65
+        self.先攻成长 = 2.30
         self.缘分列表 = []
+        self.自带战法 = Fitting_List_Enum.古之恶来

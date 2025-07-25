54f80a508f6b9e0cec0e277ab7cbe5c051c91873
zewen_zhuang <zwmacproappid@163.com>
2025-07-25 10:18:14 +0800

    refactor(普攻_soul): 重构普攻_soul类的初始化参数格式和攻击逻辑
    调整初始化参数格式以提高可读性，并修复攻击目标为空列表时的判断逻辑

--- External/Fitting/List/普攻.py
+++ External/Fitting/List/普攻.py
@@ -28,11 +28,19 @@ class 普攻_info(BaseSkillInfo):
         self.发动率 = 1
 
 class 普攻_soul(BaseSkillSoul):
-    def __init__(self, target, initiator=None, sourceType=SoulSourceType.不溯源, 
-                 skill=None, response_time=SoulResponseTime.无响应阶段, duration=-1,
-                 effect_type=SoulEffectType.无影响, effect_value=0, source_soul=None, battleField=None):
-        super().__init__(target, initiator, sourceType, skill, response_time, duration, 
-                        effect_type, effect_value, source_soul, battleField)
+    def __init__(self, 
+                target, 
+                initiator=None, 
+                sourceType=SoulSourceType.不溯源, 
+                skill=None, 
+                response_time=SoulResponseTime.无响应阶段, 
+                duration=-1,
+                effect_type=SoulEffectType.无影响, 
+                effect_value=0, 
+                source_soul=None, 
+                battleField=None):
+        super().__init__(target, initiator, sourceType, skill, response_time, 
+                    duration, effect_type, effect_value, source_soul, battleField)
 
     def response(self, status = SoulResponseTime.无响应阶段, battleField=None, hero: Hero = None, sourceSoul: Soul = None):
 
@@ -46,13 +54,18 @@ class 普攻_soul(BaseSkillSoul):
             from Calcu.JDI_Calculate import 对敌方所有目标生效, 从队列确定受击武将, 计算伤害
             attacked_heroes = 对敌方所有目标生效(self.target, battleField)
             attacked: Hero = 从队列确定受击武将(attacked_heroes, skill=self.skill, hero=self.target, battleField=battleField)
-            if attacked == None:
+            if attacked == []:
                 Log().battle_L1('[{}]没有可攻击对象'.format(self.target.get_武将名称().value))
                 return
             
             attacked_name = attacked.get_武将名称().value
             Log().battle_L1('[{}]对[{}]发动普通攻击'.format(self.target.get_武将名称().value, attacked_name))
-            damage_class: Damage = 计算伤害(battleField, self.target, attacked, SoulDamageType.兵刃, SkillType.普攻, 伤害值= 1)
+            damage_class: Damage = 计算伤害(battleField, 
+                                            self.target, 
+                                            attacked, 
+                                            SoulDamageType.兵刃, 
+                                            SkillType.普攻, 
+                                            伤害值= 1)
             damage_soul = Soul(target=attacked,
                                 initiator=self.target,
                                 sourceType=SoulSourceType.武将战法,

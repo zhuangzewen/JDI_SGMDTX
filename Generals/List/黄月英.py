0a9c9cdb98a33ae5ffdf449c655082b357f6a1c5
zhuangzewen <49627717+zhuangzewen@users.noreply.github.com>
2025-08-14 15:16:17 +0800

    黄月英

--- Generals/List/黄月英.py
+++ Generals/List/黄月英.py
@@ -7,13 +7,21 @@ class 黄月英_info(HeroInfo):
         self.武将阵营 = Faction.蜀
         self.武将兵种 = WeaponType.弓
         self.武将性别 = 0
+        
+        # 初始属性
         self.初始武力 = 31
-        self.武力成长 = 0.33
         self.初始智力 = 109
-        self.智力成长 = 2.28
         self.初始统率 = 93
-        self.统率成长 = 1.62
         self.初始先攻 = 36
+        
+        # 成长属性
+        self.武力成长 = 0.33
+        self.智力成长 = 2.28
+        self.统率成长 = 1.62
         self.先攻成长 = 2
+        
+        # 战法
         self.自带战法 = Fitting_List_Enum.木牛流马
+        
+        # 缘分
         self.缘分列表 = [BondsName_Enum.才堪相配]
